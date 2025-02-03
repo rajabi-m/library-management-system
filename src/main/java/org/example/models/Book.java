f924f5a068c2655195545bba413815f73b2668ec
Mohammad Mahdi Rajabi <m.m.rajabi.84@gmail.com>
2025-02-03 11:59:24 +0330

    add constructor and getters for Book class

--- src/main/java/org/example/models/Book.java
+++ src/main/java/org/example/models/Book.java
@@ -6,4 +6,30 @@ public class Book {
     private String author;
     private int releaseYear;
     private BookStatus status;
+
+    // Constructor
+    public Book(String title, String author, int releaseYear, BookStatus status) {
+        this.title = title;
+        this.author = author;
+        this.releaseYear = releaseYear;
+        this.status = status;
+    }
+
+    // Getters
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public BookStatus getStatus() {
+        return status;
+    }
 }
