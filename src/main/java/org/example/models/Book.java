70454da0ab1ca21e8fcf074d444aab342f36cb6a
Mohammad Mahdi Rajabi <m.m.rajabi.84@gmail.com>
2025-02-03 11:56:42 +0330

    Add Book and BookStatus classes

---
+++ src/main/java/org/example/models/Book.java
@@ -0,0 +1,9 @@
+package org.example.models;
+
+public class Book {
+    // Fields
+    private String title;
+    private String author;
+    private int releaseYear;
+    private BookStatus status;
+}
---
+++ src/main/java/org/example/models/BookStatus.java
@@ -0,0 +1,7 @@
+package org.example.models;
+
+public enum BookStatus {
+    Exist,
+    Borrowed,
+    Banned
+}
